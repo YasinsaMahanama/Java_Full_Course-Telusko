b056d6123ad979053f8543e0a60291aab5d4ce50
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-14 12:09:45 +0530

    Create Main.java

---
+++ Relational Operators/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 7;
+        int y = 5;
+
+        //boolean result = x >= y;
+        //boolean result = x <= y;
+        //boolean result = x == y;
+        //boolean result = x != y;
+        //boolean result = x > y;
+        boolean result = x < y;
+        
+        System.out.println(result);
+
+        double a = 8.8;
+        double b = 9.8;
+
+        boolean result1 = x > b;
+        
+        System.out.println(result1);
+
+    }
+
+}
