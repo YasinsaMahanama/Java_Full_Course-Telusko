1df83a8527db1cc9975b25dfda2fb7f3087efd4d
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-14 12:49:24 +0530

    Create Main.java

---
+++ If Else in Java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 8;
+        int y = 7;
+
+        if(x > 10 && x <= 20)
+            System.out.println("Hello");
+        else
+            System.out.println("Bye");
+
+        if(x > y)
+            System.out.println(x);
+        else
+            System.out.println(y);
+    }
+
+}
