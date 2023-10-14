b26e8504d895eff397e9e5971822e0c562d27b5c
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-14 10:36:45 +0530

    Create Main.java

---
+++ Data Type/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a = 5;
+        int b = 4;
+        short sh = 558;
+        long l = 5854l;
+
+        float marks = 6.5f;
+        double num = 5.9;
+
+        char c = 'k';
+
+        boolean e = true;
+        
+        System.out.println(marks);
+    }
+}
