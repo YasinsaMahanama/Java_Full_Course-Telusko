f85a4b4c9f3fa2ace3a5f93fb39d579c26f93a6c
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-14 10:57:42 +0530

    Create Main.java

---
+++ Literal in Java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 0b101;
+        int num1 = 0x7E;
+        int num2 = 10_00_00_000;
+        double num3 = 56;
+        double num4 = 12e10;
+        char c ='a';
+        c++;
+
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(c);
+    }
+}
