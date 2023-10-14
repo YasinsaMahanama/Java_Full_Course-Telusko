f92c0d4ae8337d06f31c0122844dc679d73ce325
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-14 14:56:41 +0530

    Create Main.java

---
+++ Do While Loop/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int i = 1;
+
+       do
+       {
+           System.out.println("Hi " + i);
+
+           int j = 1;
+           do
+           {
+               System.out.println("Hello " + j);
+               j++;
+           }while (j <= 3);
+           i++;
+       }while (i <=4);
+    }
+
+}
