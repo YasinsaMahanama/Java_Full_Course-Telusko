17c679b22c0300181461fae711f6bcb1e9c32b0b
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-14 15:02:25 +0530

    Create Main.java

---
+++ For Loop in Java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int i;
+
+       for(i=1;i<=4;i++)
+       {
+           System.out.println("Hi " + i);
+           int j;
+           for(j = 1;j<=3;j++)
+           {
+               System.out.println("Hello " + j);
+           }
+       }
+        System.out.println("Bye " + i);
+    }
+
+}
