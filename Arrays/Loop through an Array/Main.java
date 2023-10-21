ed31d53b8a881f6e768a2f65b1f8ac370f34ba70
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-10-22 04:13:30 +0530

    Delete Arrays/Loop through an Array/Main.java

--- Arrays/Loop through an Array/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-    for (int i = 0; i < cars.length; i++) {
-      System.out.println(cars[i]);
-    }
-  }
-}
